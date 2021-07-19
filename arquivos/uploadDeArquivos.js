70ba5c70e158e3760467f2e7be67e73e1b369a76
mari-borges <marianabrgs77@gmail.com>
2021-07-19 16:49:44 -0300

    fazendo upload de arquivos com o Stream

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,11 +1,5 @@
 const fs = require('fs')
 
-fs.readFile('./assets/cachorro.jpg', (erro, buffer)=> {
-
-    console.log('imagem foi bufferizada')
-    
-    fs.watchFile('./assets/cachorro2.jpg', buffer, (erro) =>{
-        console.log('imagem foi escrita')
-
-    })
-})
+fs.createReadStream('./assets/cachorro.jpg')
+      .pipe(fs.createWriteStream('./assets/cachorro-stream.jpg'))
+      .on('finish', () => console.log('imagem escrita com sucesso'))
