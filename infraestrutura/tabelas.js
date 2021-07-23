932108d06b63fbf988f61c38984eca9b51912484
mari-borges <marianabrgs77@gmail.com>
2021-07-23 18:30:22 -0300

    consumindo outra API dentro da API original

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -9,7 +9,7 @@ class Tabelas {
 
     criarAtendimento(){
 
-        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, nmcompleto varchar(50) NOT NULL, dsEspecialidade varchar(20) NOT NULL, data datetime, PRIMARY KEY(id))'
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, nmcompleto varchar(11) NOT NULL, dsEspecialidade varchar(20) NOT NULL, data datetime, PRIMARY KEY(id))'
 
         this.conexao.query(sql, erro => {
             if(erro) {
--- models/cadastros.js
+++ models/cadastros.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 const moment  = require('moment')
 const cadastro = require('../controllers/cadastro')
 const conexao = require('../infraestrutura/conexao')
@@ -32,11 +33,14 @@ class Cadastro {
     buscaPorId(id, res){
         const sql = `SELECT * FROM Atendimentos WHERE id=${id}`
 
-        conexao.query(sql, (erro, resultados) =>{
+        conexao.query(sql, async (erro, resultados) =>{
             const cadastro = resultados [0]
+            const cpf = cadastro.nmcompleto
             if(erro){
                 res.status(400).json(erro)
             }else {
+                const { data } = await axios.get(`http://localhost:8082/${cpf}`)
+                cadastro.nmcompleto = data
                 res.status(200).json(cadastro)
             }
         })
